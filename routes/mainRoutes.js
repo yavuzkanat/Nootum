039fadcd59ba7f641e520d1f46a94cd982993734
Ahmet <ahmetyavuzkanat@outlook.com>
2025-12-20 21:39:01 +0300

    routes for aboutus

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 const mainController = require('../controllers/main');
 
+
+// home page 
 router.get('/',mainController.mainPage);
+
+// about us
+router.get('/about',mainController.aboutPage);
 
 
 module.exports = router; 

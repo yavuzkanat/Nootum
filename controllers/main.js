967688c5880f52bd2501220300c4d3c95f5a0d79
Ahmet <ahmetyavuzkanat@outlook.com>
2025-12-20 21:41:44 +0300

    about page

--- controllers/main.js
+++ controllers/main.js
@@ -1,8 +1,17 @@
 
 
-// MAINPAGE 
+// main page
 exports.mainPage = (req,res,next) => {
     res.render('main/homepage.ejs',{
-        tabHeader : "Ana Sayfa"
+        tabHeader : "Ana Sayfa",
+        layout:'layouts/mainPagesTemplate'
     });
 };
+
+//about us
+exports.aboutPage = (req,res,next) => {
+    res.render('main/aboutusPage.ejs',{
+        tabHeader: "Biz",
+        layout:'layouts/mainPagesTemplate' 
+    });
+}

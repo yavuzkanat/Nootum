8a59bec697975910a27a1c6f3aff706944e71dc2
Ahmet <ahmetyavuzkanat@outlook.com>
2026-01-16 15:09:50 +0300

    app:remove layouts

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const express = require('express')
 const app = express()
 const mainRoutes = require('./routes/mainRoutes');
-const expressEjsLayouts = require('express-ejs-layouts');
+
 
 
 // Pug-Template
 app.set('view engine', 'ejs');
-app.use(expressEjsLayouts);
-app.set('views', './views');
+
 
 // JS & CSS files
 app.use(express.static("public"));
 
-// Public Pages
 
+// Public Pages
 app.use('',mainRoutes);
 
 app.listen(1111,() => {

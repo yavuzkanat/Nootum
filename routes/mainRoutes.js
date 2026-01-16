862952d633751b39b816624b987c283ccc378716
Ahmet <ahmetyavuzkanat@outlook.com>
2026-01-16 15:07:33 +0300

    routes:fixit

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -3,11 +3,14 @@ const router = express.Router();
 const mainController = require('../controllers/main');
 
 
-// home page 
+
+//home-page 
 router.get('/',mainController.mainPage);
 
-// about us
-router.get('/about',mainController.aboutPage);
+//about-us
+router.get('/about-us',mainController.aboutPage);
 
+//privacy
+router.get('/privacy',mainController.privacyPage);
 
 module.exports = router; 

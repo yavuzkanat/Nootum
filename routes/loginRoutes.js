0352ca6f4b6dfc8c30a9b927f54dd697ee0e4a25
Ahmet <ahmetyavuzkanat@outlook.com>
2026-01-17 11:36:39 +0300

    login register routes

---
+++ routes/loginRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const loginController = require('../controllers/login');
+
+router.get('/login',loginController.getLoginPage);
+
+module.exports = router;
